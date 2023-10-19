40d8e2263be71f05a5a49906e775ee634232ab93
VU ANH TUAN <106370295+TUAN2003@users.noreply.github.com>
2023-10-19 22:45:01 +0700

    Update ReverseInteger.java

--- src/Number/ReverseInteger.java
+++ src/Number/ReverseInteger.java
@@ -8,9 +8,6 @@ public class ReverseInteger {
             x /= 10;
         }
         if (r <= Integer.MAX_VALUE / 10 && r >= Integer.MIN_VALUE / 10) {
-            if (x > Integer.MAX_VALUE || x < Integer.MIN_VALUE) {
-                return 0;
-            } else
                 return r = r * 10 + (x % 10);
         }
         return 0;
